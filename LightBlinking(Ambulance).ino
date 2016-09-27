7137ba8a49edb3d27e91e7294b622cd27d05fe1a
Sakib Hossain <Sakibhossain.nstu@gmail.com>
2016-09-27 20:23:35 +0600

    Create LightBlinking(Ambulance).ino
    This is My First Code On Arduino

---
+++ LightBlinking(Ambulance).ino
@@ -0,0 +1,16 @@
+int ledPinG = 13;                 
+int ledPinR = 12;   
+void setup()
+{
+  pinMode(ledPinG, OUTPUT); 
+  pinMode(ledPinR, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(ledPinG, HIGH);  digitalWrite(ledPinR, LOW);   
+  delay(200);    
+  digitalWrite(ledPinR, HIGH);    digitalWrite(ledPinG, LOW);
+  delay(200);
+
+}
